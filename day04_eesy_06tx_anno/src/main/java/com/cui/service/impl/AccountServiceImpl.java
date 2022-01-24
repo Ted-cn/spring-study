d6d3040c1a70765abf1aa80ddea39deb0aa14cf7
cuizhigang <cuizg2015fly@163.com>
2022-01-24 23:52:03 +0800

    about Spring事务传播行为

--- day04_eesy_06tx_anno/src/main/java/com/cui/service/impl/AccountServiceImpl.java
+++ day04_eesy_06tx_anno/src/main/java/com/cui/service/impl/AccountServiceImpl.java
@@ -6,10 +6,11 @@ import com.cui.service.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("accountService")
-@Transactional
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 @Async
 public class AccountServiceImpl implements IAccountService {
     @Autowired
